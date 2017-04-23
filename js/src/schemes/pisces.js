96e9bce461c24b7cbc80026214868ceb847dccd1
Toolinbox <itoolinbox@gmail.com>
2017-04-23 14:23:19 +0800

    Site updated: 2017-04-23 14:23:19

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,5 +1,1 @@
-$(document).ready(function () {
-  var sidebarTop = $('.header-inner').height() + 10;
-
-  $('#sidebar').css({ 'margin-top': sidebarTop }).show();
-});
+$(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
